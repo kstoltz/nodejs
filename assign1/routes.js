edd457d2f76ab7bb9146733dc21d10fb1bc17fb7
kstoltz <46876993+kstoltz@users.noreply.github.com>
2019-01-28 21:59:04 -0500

    Add files via upload

---
+++ assign1/routes.js
@@ -0,0 +1,61 @@
+const requestHandler = (req, res) => {
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Assignment 1: User List</title></head>');
+    res.write('<body>');
+    res.write('<form action="/create-user" method="POST">');
+    res.write('<input type="text" name="userName">');
+    res.write('<button type="submit">Submit</button>');
+    res.write('<div></div>');
+    res.write('<a href=/users>List Users</a>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/users' && method === 'GET') {
+    res.write('<html>');
+    res.write('<head><title>Assignment 1: User List</title></head>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>User A</li>');
+    res.write('<li>User B</li>');
+    res.write('<li>User C</li>');
+    res.write('<li>User D</li>');
+    res.write('</ul>');
+    res.write('<div />')
+    res.write('<a href="/">Go back</a>')
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      let userName = parsedBody.split('=')[1];
+      console.log(userName);
+      
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+      
+    })
+
+  }
+
+}
+
+module.exports = {
+  handler: requestHandler
+};
